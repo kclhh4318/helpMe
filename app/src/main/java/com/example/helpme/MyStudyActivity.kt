c2254873e858aa48f117461195af0231c01fafa3
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-07-06 21:08:12 +0900

    login 수정했으요

--- app/src/main/java/com/example/helpme/MyStudyActivity.kt
+++ app/src/main/java/com/example/helpme/MyStudyActivity.kt
@@ -2,10 +2,15 @@ package com.example.helpme
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.helpme.databinding.ActivityMyStudyBinding
+import androidx.core.app.ActivityCompat
+
 
 class MyStudyActivity : AppCompatActivity() {
+
+    var backPressedTime: Long = 0
 
     private lateinit var binding: ActivityMyStudyBinding
 
@@ -25,6 +30,19 @@ class MyStudyActivity : AppCompatActivity() {
             binding.txtEmail.text = email
         } else {
             Log.e(TAG, "사용자 정보가 null입니다.")
+        }
+    }
+
+    //로그인 후 뜨는 메인 화면들이 사실상 해당 액티비티가 실질적인 메인화면이므로, 여기서 뒤로가기를 누르면 어플이 종료되는 걸로 하자.
+    @Suppress("DEPRECATION")
+    override fun onBackPressed() {
+        if (System.currentTimeMillis() - backPressedTime >= 1500) {
+            backPressedTime = System.currentTimeMillis()
+            Toast.makeText(this, "뒤로가기 버튼을 한번 더 눌러서 종료하세요.", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.finishAffinity(this)
+            System.runFinalization()
+            System.exit(0)
         }
     }
 
